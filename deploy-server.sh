b3af9a950c260a27527a16760156041904361097
Delara Aryan <aryan@usc.edu>
2021-10-17 18:21:27 -0700

    updated server deployment script

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# switch to Python 3
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+
 # install pip
 sudo apt install python-pip
 
@@ -8,7 +11,7 @@ sudo apt-get install mysql-server
 sudo apt install libmysqlclient-dev
 
 # install flask and mysql Python libraries
-sudo pip install -i https://fbsd-build.isi.deterlab.net/pypi/web/simple flask mysql flask_mysqldb
+sudo pip3 install -i https://fbsd-build.isi.deterlab.net/pypi/web/simple flask mysql flask_mysqldb
 
 # run server code
-sudo python app.py
+sudo python3 app.py
