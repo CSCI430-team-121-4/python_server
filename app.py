5a9f0929ac4a8d7111929c1c06625abbb87c1acb
Shihao Huang <huan773@usc.edu>
2021-10-17 18:43:53 -0700

    feat: almost done with transaction

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-
+import hashlib
 from flask import Flask, request, jsonify, make_response
-
 from flask_mysqldb import MySQL
+import uuid
 
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_PASSWORD'] = '84823832'
 app.config['MYSQL_DB'] = 'csci430'
 app.config['MYSQL_HOST'] = 'localhost'
 mysql = MySQL(app)
@@ -25,10 +25,19 @@ def register():
                 res = make_response("fail signing up")
                 return res
 
+        # create user
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO user(password, username) VALUES (%s, %s)", (hashing(password), username))
+        row = cur.execute("INSERT INTO user(password, username) VALUES (%s, %s)", (hashing(password), username))
         mysql.connection.commit()
         cur.close()
+        userId = cur.lastrowid
+
+        # create bank account
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO bank_account(user_id) VALUES (%s)", [userId])
+        mysql.connection.commit()
+        cur.close()
+
         res = make_response("signup succeed")
         return res
 
@@ -42,7 +51,6 @@ def login():
         cur = mysql.connection.cursor()
         # cur.execute("SELECT * FROM user WHERE username = %s AND password = %s", (username, hash(password)))
         cur.execute("SELECT * FROM user WHERE password = %s", [hashing(password)])
-        print(password,hashing(password))
         row = cur.fetchone()
         cur.close()
         print("row",row[0])
@@ -63,12 +71,21 @@ def login():
 
 @app.route('/manage')
 def manage():
-        if not request.cookies.get('auth_cookie'):
+        session = request.cookies.get('auth_cookie')
+        if session is None:
+                res = make_response("unauthorized")
                 # return unauth
-                pass
+                return res
         else:
                 # check auth
-                pass
+                cur = mysql.connection.cursor()
+                cur.execute("SELECT * FROM sessions WHERE session_id = %s", [session])
+                row = cur.fetchone()
+                if row is None:
+                        res = make_response("unauthorized")
+                        # return unauth
+                        return res
+
         # get the action and amount
         action = request.args.get('action')
         amount = request.args.get('amount')
@@ -78,12 +95,29 @@ def manage():
         if (action == "deposit"):
                 # add mySQL stuff here
                 print("deposited $" + str(amount))
+                cur = mysql.connection.cursor()
+                print(type(amount))
+                cur.execute("UPDATE bank_account SET balance = (balance + %s) WHERE user_id = %s", (amount, str(row[1])))
+                mysql.connection.commit()
+                cur.close()
         elif (action == "withdraw"):
                 # add mySQL stuff here
                 print("withdrew $" + str(amount))
+                cur = mysql.connection.cursor()
+                cur.execute("UPDATE bank_account SET balance = (balance - %s) WHERE user_id = %s", (amount, row[1]))
+                mysql.connection.commit()
+                cur.close()
         elif (action == "balance"):
                 # add mySQL stuff here
                 print("balance=")
+                cur = mysql.connection.cursor()
+                cur.execute("SELECT * FROM bank_account WHERE user_id = %s", (str(row[1])))
+                row = cur.fetchone()
+                return jsonify({
+                'Status': status_code,
+                'Response': "current balance: " + str(row[2])
+                })
+                cur.close()
         elif (action == "close"):
                 # add mySQL stuff here
                 print("account closed")
